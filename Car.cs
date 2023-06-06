185be3f32637160e021c09e88afe3a93fac5cb5a
MrHoran8120 <garyhoran@users.noreply.github.com>
2023-06-06 14:44:24 +1000

    Add project files.

---
+++ Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSerialization
+{
+    public class Car
+    {
+        public System.Drawing.Color Color { get; set; }
+        public string Rego { get; set; }
+        public string DateOfManufacture { get; set; }
+
+        public Car(Color color, string rego, string dateOfManufacture)
+        {
+            Color = Color.Black;
+            Rego = rego;
+            DateOfManufacture = dateOfManufacture;
+        }
+
+        public Car()
+        {
+        }
+
+        
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
+using TestSerialization.Properties;
+
+namespace TestSerialization
+{
+    public partial class Form1 : Form
+    {
+        List<Car> Cars = new List<Car>();
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            populateRichTextBox();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text==null) return;
+            Car theCar = new Car(colorDialog1.Color, textBox1.Text, textBox2.Text);
+            Cars.Add(theCar);
+            populateRichTextBox();
+            textBox1.Text = "";
+            textBox2.Text = "";
+        }
+
+        private void populateRichTextBox()
+        {
+            richTextBox1.Clear();
+            foreach (var theCar in Cars)
+            {
+                richTextBox1.AppendText(theCar.Rego + " ( " + theCar.DateOfManufacture + " ) " + Environment.NewLine);  
+            }
+        }
+
+        private void saveToFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Create an XmlSerializer
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Car>));
+
+            // Serialize the List<Car> to an XML file
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string theFile = Path.Combine(path, "cars.xml");
+            using (StreamWriter writer = new StreamWriter(theFile))
+            {
+                serializer.Serialize(writer, Cars);
+            }
+        }
+
+        private void loadFromFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Create an XmlSerializer
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Car>));
+
+            // Deserialize the XML file to a List<Car>
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string theFile = Path.Combine(path, "cars.xml");
+            using (StreamReader reader = new StreamReader(theFile))
+            {
+                Cars = (List<Car>)serializer.Deserialize(reader);
+            }
+
+            populateRichTextBox();
+            Console.WriteLine("List is now " + Cars.Count + Environment.NewLine);
+
+        }
+    }
+}
